5d2894c7dad87886c3d6dcb41cc17e8988581360
shecodesvietnam2018 <80838578+shecodesvietnam2018@users.noreply.github.com>
2021-04-01 11:17:22 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
-is_prime = True
+def is_prime(n):
+	flag = True
+	for i in range(2, n):
+		if n % i == 0:
+			flag = False
+			break
+			
+	if flag:
+		return True
+	else:
+		return False
+
 
 n = int(input("Enter a number: "))
 while n < 2:
 	n = int(input("Enter a number: "))
 
-for i in range(2, n):
-	if n % i == 0:
-		is_prime = False
-		break
-		
-if is_prime:
+if is_prime(n):
 	print(f"{n} is prime")
 else:
 	print(f"{n} is not prime")
